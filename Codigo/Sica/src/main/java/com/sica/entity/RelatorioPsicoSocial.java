ffcc3e369c6e9351dfe8d5e4b092758c9a76e8fc
athosxd <athosxd@gmail.com>
2012-09-17 15:15:08 -0300

    DOJO do grupo 4 com todos os membros presentes: criacao da classe RelatorioPsicoSocial

---
+++ Codigo/Sica/src/main/java/com/sica/entity/RelatorioPsicoSocial.java
@@ -0,0 +1,61 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sica.entity;
+
+import br.com.caelum.vraptor.ioc.Component;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author athos
+ */
+@Entity
+@Component
+public class RelatorioPsicoSocial {
+    @Id
+    @GeneratedValue
+    private long id;
+    //@Text => String descricao nao pode ser char(255) (?)
+    private String descricao;
+    private long idAbrigada;
+
+    public RelatorioPsicoSocial() {
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getIdAbrigada() {
+        return idAbrigada;
+    }
+
+    public void setIdAbrigada(long idAbrigada) {
+        this.idAbrigada = idAbrigada;
+    }
+
+    @Override
+    public String toString() {
+        return "RelatorioPsicoSocial{" + "id=" + id + ", descricao=" + descricao + ", idAbrigada=" + idAbrigada + '}';
+    }
+    
+    
+    
+    
+}
