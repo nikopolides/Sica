22731ce717b3aa97ad32c7c404645e3d39af1ee5
Cotrim <cotrim149@gmail.com>
2012-09-18 17:52:56 -0300

    [Cotrim] LivroDAOTest : Criado classe para teste da LivroDAO

---
+++ Codigo/Sica/src/test/java/com/sica/dao/LivroDAOTest.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sica.dao;
+
+import javax.persistence.EntityManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ * @author sica
+ */
+public class LivroDAOTest {
+    static EntityManager em;
+    
+    public LivroDAOTest() {
+    }
+    
+    @Before
+    public void setUp() {
+        
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of adiciona method, of class LivroDAO.
+     */
+    @Test
+    public void testAdiciona() {
+        
+        
+    }
+    
+    
+}
