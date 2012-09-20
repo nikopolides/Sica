ff1caf7023fa330c283f43e1ac44b5dc3588fa2c
Arthur <>
2012-09-19 23:50:58 -0300

    Arthurl e Kalil] Criação da Entidade ProcessoDependenteDAO

--- Codigo/Sica/src/main/java/com/sica/dao/ProcessoDAO.java
+++ Codigo/Sica/src/main/java/com/sica/dao/ProcessoDAO.java
@@ -7,31 +7,39 @@ package com.sica.dao;
 import br.com.caelum.vraptor.ioc.Component;
 import com.sica.entity.Abrigada;
 import com.sica.entity.Processo;
+import java.util.List;
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 /**
  *
- * @author Acer
+ * @author Kalil Mota
  */
 @Component
 public class ProcessoDAO {
 
-    private EntityManager manager;
-
-    public ProcessoDAO(EntityManager manager) {
-        this.manager = manager;
-        
+    private EntityManager entityManager;
+    
+    public ProcessoDAO(EntityManager entityManager){
+        this.entityManager = entityManager;
     }
     
+    
     public void edita(Processo processo){
-        manager.merge(processo);
-        manager.getTransaction().commit();
+        entityManager.merge(processo);
+        entityManager.getTransaction().commit();
     }
     
     public Processo findById(Processo processo){
-        return manager.find(Processo.class, processo.getId());
+        return entityManager.find(Processo.class, processo.getId());
     }
     public Abrigada findAbrigadaById(Long id){
-        return manager.find(Abrigada.class, id);
+        return entityManager.find(Abrigada.class, id);
+    }
+    public List<Processo> listaTodos(){
+        Query query = entityManager.createQuery("from processos");
+        List<Processo> lista = query.getResultList();
+        entityManager.close();
+        return lista;
     }
 }
